3024ef2937cd17137a18b252eb180953d7af6abc
Salwa-mansour <alqadasisal@gmail.com>
2023-04-27 13:21:46 +0300

    reviews error fixed

--- inc/woocommerce2.php
+++ inc/woocommerce2.php
@@ -4,7 +4,7 @@
  *
  * @link https://woocommerce.com/
  *
- * @package picworld
+ * @package bake
  */
 
 /**
@@ -19,107 +19,104 @@
 
 
 
-
-					function picworldtheme_woocommerce_setup() {
-						add_theme_support(
-							'woocommerce',
-							array(
-								'thumbnail_image_width' => 150,
-								'single_image_width'    => 300,
-								'product_grid'          => array(
-									'default_rows'    => 5,
-									'min_rows'        => 1,
-									'default_columns' => 3,
-									'min_columns'     => 2,
-									'max_columns'     => 4,
-								),
-							)
-						);
-						add_theme_support( 'wc-product-gallery-zoom' );
-						add_theme_support( 'wc-product-gallery-lightbox' );
-						add_theme_support( 'wc-product-gallery-slider' );
-
-
-						add_image_size( 'picworldtheme-slider',1920 , 800, array('center','center') );
-
-
-
-					}
-					add_action( 'after_setup_theme', 'picworldtheme_woocommerce_setup' );
-
-					/**
-					 * WooCommerce specific scripts & stylesheets.
-					 *
-					 * @return void
-					 */
-					function picworldtheme_woocommerce_scripts() {
-						wp_enqueue_style( 'picworldtheme-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION );
+	function baketheme_woocommerce_setup() {
+		add_theme_support(
+			'woocommerce',
+				array(
+					'thumbnail_image_width' => 150,
+					'single_image_width'    => 300,
+					'product_grid'          => array(
+						'default_rows'    => 5,
+						'min_rows'        => 1,
+						'default_columns' => 4,
+						'min_columns'     => 2,
+						'max_columns'     => 4,
+					),
+			)
+		);
+		add_theme_support( 'wc-product-gallery-zoom' );
+		add_theme_support( 'wc-product-gallery-lightbox' );
+		add_theme_support( 'wc-product-gallery-slider' );
+	}
+	add_action( 'after_setup_theme', 'baketheme_woocommerce_setup' );
+	
+	/**
+	 * WooCommerce specific scripts & stylesheets.
+	 *
+	 * @return void
+	 */
+	function baketheme_woocommerce_scripts() {
+			wp_enqueue_style( 'picworldtheme-woocommerce-style', get_template_directory_uri() . '/woocommerce.css', array(), _S_VERSION );
 							wp_enqueue_style( 'picworldtheme-woocommerce-style-rtl', get_template_directory_uri() . '/woocommerce-rtl.css', array(), _S_VERSION );
 
-						$font_path   = WC()->plugin_url() . '/assets/fonts/';
-						$inline_font = '@font-face {
-								font-family: "star";
-								src: url("' . $font_path . 'star.eot");
-								src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-									url("' . $font_path . 'star.woff") format("woff"),
-									url("' . $font_path . 'star.ttf") format("truetype"),
-									url("' . $font_path . 'star.svg#star") format("svg");
-								font-weight: normal;
-								font-style: normal;
-							}';
+		$font_path   = WC()->plugin_url() . '/assets/fonts/';
+		$inline_font = '@font-face {
+				font-family: "star";
+				src: url("' . $font_path . 'star.eot");
+				src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
+					url("' . $font_path . 'star.woff") format("woff"),
+					url("' . $font_path . 'star.ttf") format("truetype"),
+					url("' . $font_path . 'star.svg#star") format("svg");
+				font-weight: normal;
+				font-style: normal;
+			}';
+	
+		wp_add_inline_style( 'baketheme-woocommerce-style', $inline_font );
+	}
+	add_action( 'wp_enqueue_scripts', 'baketheme_woocommerce_scripts' );
+	
+	/**
+	 * Disable the default WooCommerce stylesheet.
+	 *
+	 * Removing the default WooCommerce stylesheet and enqueing your own will
+	 * protect you during WooCommerce core updates.
+	 *
+	 * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
+	 */
+//	add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+	
+	/**
+	 * Add 'woocommerce-active' class to the body tag.
+	 *
+	 * @param  array $classes CSS classes applied to the body tag.
+	 * @return array $classes modified to include 'woocommerce-active' class.
+	 */
+	function baketheme_woocommerce_active_body_class( $classes ) {
+		$classes[] = 'woocommerce-active';
+	
+		return $classes;
+	}
+	add_filter( 'body_class', 'baketheme_woocommerce_active_body_class' );
 
-						wp_add_inline_style( 'picworldtheme-woocommerce-style', $inline_font );
-					}
-					add_action( 'wp_enqueue_scripts', 'picworldtheme_woocommerce_scripts' );
 
-					/**
-					 * Disable the default WooCommerce stylesheet.
-					 *
-					 * Removing the default WooCommerce stylesheet and enqueing your own will
-					 * protect you during WooCommerce core updates.
-					 *
-					 * @link https://docs.woocommerce.com/document/disable-the-default-stylesheet/
-					 */
-					// add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
-					/**
-					 * Add 'woocommerce-active' class to the body tag.
-					 *
-					 * @param  array $classes CSS classes applied to the body tag.
-					 * @return array $classes modified to include 'woocommerce-active' class.
-					 */
-					function picworldtheme_woocommerce_active_body_class( $classes ) {
-						$classes[] = 'woocommerce-active';
+	if ( ! function_exists( 'baketheme_woocommerce_cart_link_fragment' ) ) {
+		/**
+		 * Cart Fragments.
+		 *
+		 * Ensure cart contents update when products are added to the cart via AJAX.
+		 *
+		 * @param array $fragments Fragments to refresh via AJAX.
+		 * @return array Fragments to refresh via AJAX.
+		 */
+		function baketheme_woocommerce_cart_link_fragment( $fragments ) {
+			global $woocommerce;
+			ob_start();
+			?>
+			<span class="items-count"><?php echo  WC()->cart->get_cart_contents_count(); ?></span>
+			<?php
+			// baketheme_woocommerce_cart_link();
+			$fragments['.cart-item .items-count'] = ob_get_clean();
+		
+	
+			return $fragments;
+		}
+	}
+	add_filter( 'woocommerce_add_to_cart_fragments', 'baketheme_woocommerce_cart_link_fragment' );
 
-						return $classes;
-					}
-					add_filter( 'body_class', 'picworldtheme_woocommerce_active_body_class' );
-
-					/**
-					 * Related Products Args.
-					 *
-					 * @param array $args related products args.
-					 * @return array $args related products args.
-					 */
-					function picworldtheme_woocommerce_related_products_args( $args ) {
-						$defaults = array(
-							'posts_per_page' => 3,
-							'columns'        => 3,
-						);
-
-						$args = wp_parse_args( $defaults, $args );
-
-						return $args;
-					}
-					add_filter( 'woocommerce_output_related_products_args', 'picworldtheme_woocommerce_related_products_args' );
-
-					/**
-					 * Remove default WooCommerce wrapper.
-					 */
-					remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
+		remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 					remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-
-					if ( ! function_exists( 'picworldtheme_woocommerce_wrapper_before' ) ) {
+							if ( ! function_exists( 'picworldtheme_woocommerce_wrapper_before' ) ) {
 						/**
 						 * Before Content.
 						 *
@@ -151,41 +148,7 @@
 					}
 					add_action( 'woocommerce_after_main_content', 'picworldtheme_woocommerce_wrapper_after' );
 
-					
-				
-
-					if ( ! function_exists( 'picworldtheme_woocommerce_header_cart' ) ) {
-						/**
-						 * Display Header Cart.
-						 *
-						 * @return void
-						 */
-						function picworldtheme_woocommerce_header_cart() {
-							if ( is_cart() ) {
-								$class = 'current-menu-item';
-							} else {
-								$class = '';
-							}
-							?>
-							<ul id="site-header-cart" class="site-header-cart">
-								<li class="<?php echo esc_attr( $class ); ?>">
-									<?php picworldtheme_woocommerce_cart_link(); ?>
-								</li>
-								<li>
-									<?php
-									$instance = array(
-										'title' => '',
-									);
-
-									the_widget( 'WC_Widget_Cart', $instance );
-									?>
-								</li>
-							</ul>
-							<?php
-						}
-					}
-
-					 function is_on_sale( $context = 'view' ) {
+							 function is_on_sale( $context = 'view' ) {
 					   if ( '' !== (string) $context->get_sale_price( $context ) && $context->get_regular_price( $context ) > $context->get_sale_price( $context ) ) {
 						   $on_sale = true;
 
@@ -205,7 +168,7 @@
 						// include_once get_template_directory() . 'wp-content/plugins/woocommerce/includes/abstracts/abstract-wc-product.php';
 						// include_once	get_template_directory() . '/woocommerce/onSale.php';
 					}
-					// /---------------------------------------------------------------------
+								// /---------------------------------------------------------------------
 					add_action( 'picworldtheme_get_sale_product','picworldtheme_get_sale_items',10 );
 
 					add_filter( 'woocommerce_single_product_carousel_options', 'sf_update_woo_flexslider_options' );
@@ -230,15 +193,9 @@
 
 					    return $options;
 					}
-					// ------------------------------
-					// function picworldtheme_woocommerce_template_loop_product_galery_img($product)
-					// {
-					// $attachment_ids =get_gallery_image_ids($product);
-					// $firest_imgIid =$attachment_ids[0];
-					// $img =get_gallery_image_html($firest_imgIid );
-					// 	echo img;
-					// }
-function picworldtheme_filter_link()
+
+
+					function picworldtheme_filter_link()
 {
 	echo '<div  id="fillter-btn" role="button" rel="filters" ><span>'.esc_html('show fillters').'</span>
 	<svg width="700pt" height="700pt" version="1.1" viewBox="0 -70 700 700" id="filters-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -269,7 +226,9 @@ function picworldtheme_filter_link()
 					if(! is_front_page()):
 					add_action('woocommerce_after_shop_loop_item_title','the_excerpt',1);
 					endif;
-					// if(is_shop()){
+
+
+										// if(is_shop()){
 
 					// 	add_action("woocommerce_before_shop_loop_item_title","picworldtheme_woocommerce_template_loop_product_galery_img",11,1);
 						
@@ -308,14 +267,10 @@ add_action( 'woocommerce_before_shop_loop_item','picworldtheme_slider_start',12)
 add_action( 'woocommerce_before_shop_loop_item_title','picworldtheme_slider_thumbnail_wrap_start', 7);
 			// woocommerce_before_shop_loop_item_title, woocommerce_template_loop_product_thumbnail,	10	
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash',10);
-
 add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_show_product_loop_sale_flash',11);
 add_action( 'woocommerce_before_shop_loop_item_title','picworldtheme_slider_thumbnail_wrap_end', 13);
 add_action( 'woocommerce_before_shop_loop_item_title','picworldtheme_get_firest_gallery_image', 15);
 add_action( 'woocommerce_before_shop_loop_item_title','picworldtheme_slider_end',20);
-
-// remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating',6);
-remove_action ('woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
 
 function picworldtheme_get_copons(){
 	 global $woocommerce;
@@ -324,5 +279,3 @@ function picworldtheme_get_copons(){
 }
 
 // add_action('wp_body_open','picworldtheme_get_copons');
-
-	// .........................
